7deef1649a522d275b907a5fb45c288982032899
Bogdan21o312 <lutskiv2010@gmail.com>
2023-06-03 18:33:39 +0300

    Update

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. At autem consequatur, dolore ex expedita fugit illum in inventore minima nam obcaecati quisquam reiciendis, rerum, sint tempore ullam voluptatum. Aliquam, dolorem?
+    </div>
+  )
+}
